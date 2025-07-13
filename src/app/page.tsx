e8b2e5158891e3973d1203f151a64ca9a818aa4a
Tokosama <anouartoko@gmail.com>
2025-07-13 14:24:43 +0100

    Handled of the WhoToFollow interface, et the follow/unfollow functionnality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,15 @@ import {
 import Image from "next/image";
 import { currentUser } from "@clerk/nextjs/server";
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 
 export default async function Home() {
-
   const user = await currentUser();
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-      <div className="lg:col-span-6">
-
-
-{user?<CreatePost /> :null }
-        
-      </div>
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        WhoToFollow
+        <WhoToFollow />
       </div>
     </div>
   );
